68f7c158bbd714e0317efa16f1af219714f35b8b
amritaahan18 <amritaahan1305@gmail.com>
2025-11-07 11:47:41 +0530

    Add Course class for Part A - Dependency Injection

---
+++ src/main/java/com/exp/parta/Course.java
@@ -0,0 +1,38 @@
+package com.exp.parta;
+
+public class Course {
+    private String courseName;
+    private String courseCode;
+
+    public Course() {
+    }
+
+    public Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", courseCode='" + courseCode + '\'' +
+                '}';
+    }
+}
